8f31e467bc43e1c6ded316a47e54216fb7e72ad3
ReBlast <reblastoff@gmail.com>
2024-03-11 19:35:50 +0500

    Fix version number. Remove unnecessary code.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,25 +4,13 @@ plugins {
 }
 
 group = "me.blast"
-version = "0.1"
+version = "0.1.0"
 
 repositories {
     mavenCentral()
 }
 
 kotlin {
-    val hostOs = System.getProperty("os.name")
-    val isArm64 = System.getProperty("os.arch") == "aarch64"
-    val isMingwX64 = hostOs.startsWith("Windows")
-    val nativeTarget = when {
-        hostOs == "Mac OS X" && isArm64 -> macosArm64("native")
-        hostOs == "Mac OS X" && !isArm64 -> macosX64("native")
-        hostOs == "Linux" && isArm64 -> linuxArm64("native")
-        hostOs == "Linux" && !isArm64 -> linuxX64("native")
-        isMingwX64 -> mingwX64("native")
-        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
-    }
-    
     sourceSets {
         val nativeMain by getting
         val nativeTest by getting
