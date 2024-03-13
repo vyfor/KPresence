811e452c0a6bfd2ff75bfd00eeb419b9a08ce184
ReBlast <reblastoff@gmail.com>
2024-03-13 23:20:51 +0500

    Prepare for publishing to Sonatype. Add Dokka.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,64 +1,136 @@
+import org.jetbrains.dokka.gradle.DokkaTask
+import java.util.Base64
+
 plugins {
     kotlin("multiplatform") version "1.9.22"
     kotlin("plugin.serialization") version "1.9.22"
+    id("org.jetbrains.dokka") version "1.9.20"
     id("maven-publish")
+    id("signing")
+    id("com.louiscad.complete-kotlin") version "1.1.0"
 }
 
 group = "me.blast"
-version = "0.1.2"
+version = "0.1.3"
 
 repositories {
     mavenCentral()
 }
 
 kotlin {
-    val hostOs = System.getProperty("os.name")
-    val isArm64 = System.getProperty("os.arch") == "aarch64"
-    val isMingwX64 = hostOs.startsWith("Windows")
-    when {
-        hostOs == "Mac OS X" && isArm64 -> macosArm64("native")
-        hostOs == "Mac OS X" && !isArm64 -> macosX64("native")
-        hostOs == "Linux" && isArm64 -> linuxArm64("native")
-        hostOs == "Linux" && !isArm64 -> linuxX64("native")
-        isMingwX64 -> mingwX64("native")
-        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
-    }
+    val targets = listOf(
+        mingwX64(),
+        linuxX64(),
+        linuxArm64(),
+        macosX64(),
+        macosArm64()
+    )
     
     sourceSets {
-        val nativeMain by getting
-        val nativeTest by getting
-        
-        nativeMain.dependencies {
-            implementation(kotlin("stdlib"))
-            implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
-            implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
+        val commonMain by getting
+        val nativeMain by creating {
+            dependsOn(commonMain)
+            
+            dependencies {
+                implementation(kotlin("stdlib"))
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
+            }
+        }
+        val nativeTest by creating {
+            dependencies {
+                implementation(kotlin("test"))
+            }
         }
         
-        nativeTest.dependencies {
-            implementation(kotlin("test"))
+        targets.forEach { target ->
+            target.compilations["main"].defaultSourceSet.dependsOn(nativeMain)
+        }
+        
+        targets.forEach { target ->
+            target.compilations["test"].defaultSourceSet.dependsOn(nativeTest)
         }
     }
+}
+
+val dokkaOutputDir = "$buildDir/dokka"
+
+tasks.getByName<DokkaTask>("dokkaHtml") {
+    outputDirectory.set(file(dokkaOutputDir))
+}
+
+val deleteDokkaOutputDir by tasks.register<Delete>("deleteDokkaOutputDirectory") {
+    delete(dokkaOutputDir)
+}
+
+val javadocJar = tasks.register<Jar>("javadocJar") {
+    dependsOn(deleteDokkaOutputDir, tasks.dokkaHtml)
+    archiveClassifier.set("javadoc")
+    from(dokkaOutputDir)
 }
 
 publishing {
     repositories {
         maven {
             name = "GitHubPackages"
-            setUrl("https://maven.pkg.github.com/reblast/KPresence")
+            url = uri("https://maven.pkg.github.com/reblast/KPresence")
             credentials {
                 username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_ACTOR")
                 password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
             }
         }
-    }
-    publications {
-        if (findByName("kotlinMultiplatform") == null) {
-            create<MavenPublication>("kotlinMultiplatform") {
-                from(components["kotlinMultiplatform"])
-                groupId = "me.blast"
-                artifactId = "kpresence"
-                version = "0.1.2"
+        
+        maven {
+            name = "OSS"
+            val releasesRepoUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deployByRepositoryId/${project.findProperty("sonatype.repository")}/")
+            val snapshotsRepoUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+            url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+            
+            credentials {
+                username = project.findProperty("sonatype.username") as String? ?: System.getenv("SONATYPE_USERNAME")
+                password = project.findProperty("sonatype.password") as String? ?: System.getenv("SONATYPE_PASSWORD")
             }
         }
     }
+    
+    publications {
+        withType<MavenPublication> {
+            artifact(javadocJar)
+            
+            pom {
+                groupId = "me.blast"
+                artifactId = "kpresence"
+                version = project.version.toString()
+                name.set("KPresence")
+                description.set("A lightweight, cross-platform Kotlin library for Discord Rich Presence interaction.")
+                url.set("https://github.com/reblast/KPresence")
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+                developers {
+                    developer {
+                        name.set("axeon")
+                        email.set(project.findProperty("sonatype.email") as String)
+                        url.set("https://github.com/reblast")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/reblast/KPresence.git")
+                    developerConnection.set("scm:git:ssh://github.com:reblast/KPresence.git")
+                    url.set("http://github.com/reblast/KPresence/tree/master")
+                }
+            }
+        }
+    }
+}
+
+signing {
+    useInMemoryPgpKeys(
+        Base64.getDecoder().decode(project.findProperty("gpg.key") as String? ?: System.getenv("GPG_PRIVATE_KEY")).decodeToString(),
+        project.findProperty("gpg.password") as String? ?: System.getenv("GPG_PRIVATE_PASSWORD")
+    )
+    sign(publishing.publications)
 }
