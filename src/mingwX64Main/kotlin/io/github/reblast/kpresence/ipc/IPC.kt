17b50bc9d1525eaa21efddbf399d03decf9f19be
ReBlast <reblastoff@gmail.com>
2024-03-18 16:11:19 +0500

    Fix formatting.

--- src/mingwX64Main/kotlin/io/github/reblast/kpresence/ipc/IPC.kt
+++ src/mingwX64Main/kotlin/io/github/reblast/kpresence/ipc/IPC.kt
@@ -2,10 +2,12 @@ package io.github.reblast.kpresence.ipc
 
 import io.github.reblast.kpresence.utils.putInt
 import io.github.reblast.kpresence.utils.reverseBytes
-import kotlinx.cinterop.ExperimentalForeignApi
-import kotlinx.cinterop.refTo
+import kotlinx.cinterop.*
+import platform.posix.close
+import platform.posix.errno
 import platform.posix.read
 import platform.posix.write
+import platform.windows.*
 
 @OptIn(ExperimentalForeignApi::class)
 actual fun readBytes(handle: Int, bufferSize: Int): ByteArray {
@@ -20,13 +22,13 @@ actual fun readBytes(handle: Int, bufferSize: Int): ByteArray {
 @OptIn(ExperimentalForeignApi::class)
 actual fun writeBytes(handle: Int, opcode: Int, data: String) {
   if (handle == -1) throw IllegalStateException("Not connected")
-  
+
   val bytes = data.encodeToByteArray()
   val buffer = ByteArray(bytes.size + 8)
-  
+
   buffer.putInt(opcode.reverseBytes())
   buffer.putInt(bytes.size.reverseBytes(), 4)
   bytes.copyInto(buffer, 8)
-  
+
   write(handle, buffer.refTo(0), buffer.size.toUInt())
 }
