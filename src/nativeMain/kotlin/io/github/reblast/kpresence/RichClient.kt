c99a75b9ce7832fb6625649b004ea53feb649ca6
ReBlast <reblastoff@gmail.com>
2024-03-19 20:55:17 +0500

    Add state and reconnection logic. Prevent identical updates.

--- src/nativeMain/kotlin/io/github/reblast/kpresence/RichClient.kt
+++ src/nativeMain/kotlin/io/github/reblast/kpresence/RichClient.kt
@@ -14,41 +14,62 @@ import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import platform.posix.close
 import platform.posix.getpid
+import kotlin.io.print
 
 /**
  * Manages client connections and activity updates for Discord presence.
  * @property clientId The Discord application client ID.
  */
 class RichClient(val clientId: Long) {
+  var state = State.DISCONNECTED
   var handle = -1
     private set
   
   private val clientScope = CoroutineScope(Dispatchers.IO)
-  private val updateInterval = 4000L
+  private val updateInterval = 15000L
   private var lastActivity: Activity? = null
   private var lastUpdated = 0L
   private var updateTimer: Job? = null
 
   /**
    * Establishes a connection to Discord.
-   * Attempts to reconnect if there is an already active connection.
    * @param callback The callback function to be executed after establishing the connection.
    * @return The current Client instance for chaining.
    */
   fun connect(callback: (RichClient.() -> Unit)? = null): RichClient {
-    if (handle != -1) close(handle)
+    if (state != State.DISCONNECTED) return this
+
     handle = openPipe()
+    state = State.CONNECTED
     handshake()
+
     callback?.invoke(this)
+
     return this
   }
-  
+
+  /**
+   * Attempts to reconnect if there is an already active connection.
+   * @return The current Client instance for chaining.
+   */
+  fun reconnect(): RichClient {
+    require(state != State.DISCONNECTED) { "Reconnection is not possible while disconnected." }
+
+    shutdown()
+    connect()
+
+    return this
+  }
+
   /**
    * Updates the current activity shown on Discord.
+   * Skips identical presence updates.
    * @param activity The activity to display.
    * @return The current Client instance for chaining.
    */
-  fun update(activity: Activity): RichClient {
+  fun update(activity: Activity?): RichClient {
+    require(state == State.SENT_HANDSHAKE) { "Presence updates are not allowed while disconnected." }
+    if (lastActivity == activity) return this
     lastActivity = activity
     val currentTime = epochMillis()
     val timeSinceLastUpdate = currentTime - lastUpdated
@@ -71,9 +92,8 @@ class RichClient(val clientId: Long) {
    * @return The current Client instance for chaining.
    */
   fun clear(): RichClient {
-    val packet = Json.encodeToString(Packet("SET_ACTIVITY", PacketArgs(getpid(), null), "-"))
-    writeBytes(handle, 1, packet)
-    readBytes(handle)
+    require(state == State.SENT_HANDSHAKE) { "Cannot clear presence while disconnected." }
+    update(null)
     return this
   }
   
@@ -82,26 +102,40 @@ class RichClient(val clientId: Long) {
    * @return The current Client instance for chaining.
    */
   fun shutdown(): RichClient {
+    if (state == State.DISCONNECTED) return this
     // TODO: Send valid payload
-    writeBytes(handle, 2, "")
+    writeBytes(handle, 2, "{\"v\": 1,\"client_id\":\"$clientId\"}")
+    readBytes(handle)
     close(handle)
+    state = State.DISCONNECTED
     handle = -1
     updateTimer?.cancel()
     updateTimer = null
     lastActivity = null
+    lastUpdated = 0
     return this
   }
   
   private fun sendActivityUpdate() {
+    if (state != State.SENT_HANDSHAKE) return
     val packet = Json.encodeToString(Packet("SET_ACTIVITY", PacketArgs(getpid(), lastActivity), "-"))
     writeBytes(handle, 1, packet)
-  }
-  
-  /**
-   * Performs the initial handshake with Discord.
-   */
-  private fun handshake() {
-    writeBytes(handle, 0, "{\"v\": 1,\"client_id\":\"$clientId\"}")
     readBytes(handle)
   }
+  
+  private fun handshake() {
+    writeBytes(handle, 0, "{\"v\": 1,\"client_id\":\"$clientId\"}")
+    if (readBytes(handle).decodeToString().contains("Invalid client ID")) {
+      throw RuntimeException("Provided invalid client ID: $clientId")
+    }
+    state = State.SENT_HANDSHAKE
+  }
 }
+
+enum class State {
+  DISCONNECTED,
+  CONNECTED,
+  SENT_HANDSHAKE,
+}
+
+
