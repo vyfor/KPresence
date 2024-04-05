6658e55fdf71adb7253305cfe64be136387be95d
ReBlast <reblastoff@gmail.com>
2024-04-06 00:25:59 +0500

    Proper way to obtain process id.

--- src/jvmMain/kotlin/io/github/reblast/kpresence/utils/Utils.kt
+++ src/jvmMain/kotlin/io/github/reblast/kpresence/utils/Utils.kt
@@ -2,13 +2,6 @@
 
 package io.github.reblast.kpresence.utils
 
-import java.lang.management.ManagementFactory
-
 actual fun epochMillis(): Long = System.currentTimeMillis()
 
-actual fun getProcessId(): Int = ManagementFactory
-  .getRuntimeMXBean()
-  .name
-  .let { it.substring(0, it.indexOf("@")) }
-  .toIntOrNull() ?:
-  throw RuntimeException("Could not obtain process id")
+actual fun getProcessId(): Int = ProcessHandle.current().pid().toInt()
