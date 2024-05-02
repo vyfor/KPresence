691617b4a03a5bf452903a33274b5eab9707cbca
ReBlast <reblastoff@gmail.com>
2024-05-02 20:59:24 +0500

    refactor: remove unused functions

--- src/commonMain/kotlin/io/github/vyfor/kpresence/utils/Utils.kt
+++ src/commonMain/kotlin/io/github/vyfor/kpresence/utils/Utils.kt
@@ -7,26 +7,6 @@ internal fun ByteArray.putInt(value: Int, offset: Int = 0) {
   this[offset + 3] = value.toByte()
 }
 
-internal fun ByteArray.putIntLE(value: Int, offset: Int = 0) {
-  this[offset] = value.toByte()
-  this[offset + 1] = (value shr 8).toByte()
-  this[offset + 2] = (value shr 16).toByte()
-  this[offset + 3] = (value shr 24).toByte()
-}
-
-internal fun ByteArray.putIntBE(value: Int, offset: Int = 0) {
-  this[offset] = (value shr 24).toByte()
-  this[offset + 1] = (value shr 16).toByte()
-  this[offset + 2] = (value shr 8).toByte()
-  this[offset + 3] = value.toByte()
-}
-
-internal fun ByteArray.getInt(offset: Int = 0): Int {
-  return (this[offset].toInt() shl 24) or
-    (this[offset + 1].toInt() shl 16) or
-    (this[offset + 2].toInt() shl 8) or
-    (this[offset + 3].toInt())
-}
 
 internal fun Int.reverseBytes(): Int {
   return (this and -0x1000000 ushr 24) or
