69067f396bb04e8010417f452cbd4801cef59651
vyfor <vyforium@gmail.com>
2025-03-10 20:46:44 +0500

    test: fix invalid client id

--- src/commonTest/kotlin/io/github/vyfor/kpresence/ClientTest.kt
+++ src/commonTest/kotlin/io/github/vyfor/kpresence/ClientTest.kt
@@ -8,38 +8,42 @@ import kotlin.test.Test
 class ClientTest {
   @Test
   fun testClient() {
-    val client = RichClient(1216296290451984424)
+    val client = RichClient(1219918880005165137)
     client.logger = ILogger.default(LogLevel.TRACE)
-    
-    client.on<ReadyEvent> {
-      client.logger?.info("Awaiting input (<text>, clear, reconnect, reconnectAsync, shutdown):")
-    }.connect(true)
-    
+
+    client
+            .on<ReadyEvent> {
+              client.logger?.info(
+                      "Awaiting input (<text>, clear, reconnect, reconnectAsync, shutdown):"
+              )
+            }
+            .connect(true)
+
     while (true) {
       val input = readln()
-      
+
       if (input == "shutdown") {
         client.shutdown()
         break
       }
-      
+
       if (input == "reconnect") {
         client.reconnect(true)
         continue
       }
-      
+
       if (input == "reconnectAsync") {
         client.reconnect(false)
         continue
       }
-      
+
       if (client.connectionState == ConnectionState.DISCONNECTED) break
-      
+
       if (input == "clear") {
         client.clear()
         continue
       }
-      
+
       client.update {
         details = input
         state = "KPresence"
